f50c97aca882973c5f0f49f8deed21a59ce069a4
lo <louelp24@gmail.com>
2022-10-13 20:11:06 +0800

    added handle change on input field and handle submit to the form element. Pass states of todo, setTodo, todoList, setTodoList as props from app.js so it can be access if form component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
 function App() {
+  // when rendering a list and distributing it to other component. It is necessary to put it into the main component(App.js), so that we can pass it as props to any component that we want because, all of the component is connected to the main component. That's why the todoList is here.
+  
+  const [ todo, setTodo ] = useState('');
+  const [ todoList, setTodoList ] = useState([]);
+
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,27 @@
 import styles from "../style.module.css";
 
-const Form = () => {
+const Form = ({ todo, setTodo, todoList, setTodoList }) => {
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTodoList([...todoList, todo])
+    }
     return (
         <div className={styles.todoForm}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
+                    value={todo}
+                    onChange={handleChange}
                     className={styles.todoInput}
                     placeholder="Add Todo Item"
                 />
-                <button className={styles.todoButton}>Add</button>
+                <button 
+                    type="subimit"
+                    className={styles.todoButton}
+                >Add
+                </button>
             </form>
         </div>
     )
