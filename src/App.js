30f1ef9b8ac2c8769354b3e426f92c3602fb06f8
lo <louelp24@gmail.com>
2022-10-13 20:28:55 +0800

    created todo component and put module.css styles in it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import Todo from "./components/Todo";
 
 function App() {
   // when rendering a list and distributing it to other component. It is necessary to put it into the main component(App.js), so that we can pass it as props to any component that we want because, all of the component is connected to the main component. That's why the todoList is here.
-  
+
   const [ todo, setTodo ] = useState('');
   const [ todoList, setTodoList ] = useState([]);
 
@@ -17,6 +18,7 @@ function App() {
         todoList={todoList}
         setTodoList={setTodoList}
       />
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import styles from "../style.module.css";
+
+
+const Todo = () => {
+    return (
+        <div>
+            <div className={styles.todoItem}>
+                <h3 className={styles.todoName}>Todo Item</h3>
+                <button className={styles.deleteButton}>Done</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
